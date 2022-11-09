35b664177f4c9ba778b66751afd5673e95c6a455
Bar Nuri <barnuri@hotmail.com>
2022-11-09 10:41:52 +0200

    Delete profile.ps1

--- profile.ps1
+++
@@ -1,2 +0,0 @@
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
-$(Invoke-WebRequest https://raw.githubusercontent.com/barnuri/powershell-utils/master/installProfileTools.ps1 -Headers @{"Cache-Control"="no-cache"}).Content | iex
