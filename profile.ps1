0f64f393b201f614a2744a43c91ee61a3e9ea34c
Bar Nuri <barnuri@hotmail.com>
2022-11-09 10:27:57 +0200

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,2 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+$(Invoke-WebRequest https://raw.githubusercontent.com/barnuri/powershell-utils/master/installProfile.ps1 -Headers @{"Cache-Control"="no-cache"}).Content | iex
