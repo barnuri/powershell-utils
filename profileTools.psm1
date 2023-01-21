6f4ab5f10a507cb63c2cc07f1e44dba16b69b227
Bar Nuri <barnuri@hotmail.com>
2023-01-21 21:35:40 +0200

    Update profileTools.psm1

--- profileTools.psm1
+++ profileTools.psm1
@@ -13,7 +13,7 @@ function profileTools() {
 function syncPowershellUtils() {
     mkdir -p (Split-Path -Path $profile -Parent) -errorAction SilentlyContinue
     $date = $(Get-Date).ToString()
-    $newProfileContent = $(curl https://raw.githubusercontent.com/barnuri/powershell-utils/master/profileTools.psm1?noCache=$date -H "Cache-Control: no-cache, no-store, must-revalidate"-H "Pragma: no-cache")
+    $newProfileContent = $(Invoke-WebRequest https://raw.githubusercontent.com/barnuri/powershell-utils/master/profileTools.psm1?noCache=$date).Content
     echo $newProfileContent > $profileTools
     $installString = "### load profileTools.psm1"
     $importModuleExists = Select-String -Quiet -Pattern $installString -Path $profile
